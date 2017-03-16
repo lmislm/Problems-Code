835bc7ad86637afa9760e1ac7f65842d62c320fe
lmislm <lmjustlm@163.com>
2017-03-16 20:28:34 +0800

    增加方法三

--- codewars-accm.js
+++ codewars-accm.js
@@ -15,8 +15,23 @@ function accum(s) {
     }
     return s.join("-");
 }
+
+//
+function accum(s){
+    var letter = s.split('');
+    var result = [];
+    for(var i=0;i<letter.length;i++){
+        result.push(letter[i].toUpperCase()+Array(i+1).join(letter[i].toLowerCase()));
+    }
+    return result.join('_');
+}
+//输出
+accum('fds')
+F_Dd_Sss
+
 //参考的js方法，其中对
 //
 // function accm(s){
 //     return s.split('').map((x,index) => x.toUpperCase() + Array(index+1).join(x.toLowerCase())).join('-');
 // }
+//方法三
