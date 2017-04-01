4aec08f76a8724682b8a4faa20c65c0ea2f54085
lmislm <lmjustlm@163.com>
2017-04-01 21:47:57 +0800

    简化空间复杂度，只用一个for循环

--- codewars_Vowel Count.js
+++ codewars_Vowel Count.js
@@ -1,16 +1,28 @@
 /**
  * Created by Administrator on 2017/4/1.
  */
-function getCount(str) {
-    var vowelsCount = 0;
-        var a = ['a','e','i','o','u'];
-        for(var i=0; i<str.length; i++){
-            for(var j=0;j<a.length; j++){
-                if(a[j]===str[i]){
-                    vowelsCount ++;
-                }
-            }
-        }
-    return vowelsCount;
-}
-console.log(getCount('jjjj'));
+// function getCount(str) {
+//     var vowelsCount = 0;
+//         var a = ['a','e','i','o','u'];
+//         for(var i=0; i<str.length; i++){
+//             for(var j=0;j<a.length; j++){
+//                 if(a[j]===str[i]){
+//                     vowelsCount ++;
+//                 }
+//             }
+//         }
+//     return vowelsCount;
+// }
+// console.log(getCount('jjjj'));
+
+// function getCount(str) {
+//     var vowelCount = 0;
+//     var a = ['a','e','i','o','u'];
+//     for(var i=0;i<str.length; i++){
+//         if((a.indexOf(str[i]))!==-1){
+//             vowelCount ++;
+//         }
+//     }
+//     return vowelCount;
+// }
+// console.log(getCount('aeyou'));
