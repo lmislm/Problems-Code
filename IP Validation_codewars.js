04d58715b9768c68c9a63f483833a047ec0be292
lmislm <lmjustlm@users.noreply.github.com>
2018-02-10 14:53:02 +0800

    IP Validation

---
+++ IP Validation_codewars.js
@@ -0,0 +1,22 @@
+/**
+ * Created by lmislm on 2018/2/10.
+ * Github：https://github.com/lmislm/
+ */
+function isValidIP(str) {
+    var arr = str.split('.');
+
+    return arr.filter(function (value) {
+        //Number
+        return value == Number(value).toString() && Number(value) <= 255;
+    }).length==4;
+}
+//这个还要自己写测试代码，晕，大概这么个意思
+// var
+//     v,
+//     x,
+//     arr = [];
+// for(x=1; x<100; x++) {
+//     arr.push(x);
+// }
+// r = isValidIP(str);
+// if()
---
+++ 进制转换.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Administrator on 2018/2/8.
+ */
+function turnbased(num, base) {
+    var s = new Stack();
+    do{
+        s.push(num % base);
+        num = Math.floor(num /= base);
+    }while(num>0);
+    var converted = "";
+    while (s.length() > 0){
+        converted += s.pop();
+    }
+    return converted;
+}
