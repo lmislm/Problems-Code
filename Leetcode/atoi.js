0b3e08e644299b13f9463596fb3bb641fc6eaa3f
lmislm <wszdlm@gmail.com>
2018-03-02 22:04:20 +0800

    finish

--- Leetcode/atoi.js
+++ Leetcode/atoi.js
@@ -2,44 +2,36 @@
  * @Author: lmislm 
  * @Date: 2018-03-02 16:03:40 
  * @Last Modified by: lmislm
- * @Last Modified time: 2018-03-02 20:13:26
+ * @Last Modified time: 2018-03-02 22:03:43
  */
 var myAtoi = function (str) {   
     //return str.trim().match(/^([+-])?([0-9]+)/m).shift().toString();
     //slice in leetcode cant read property
 
-        // var arr = str.trim().match(/^([+-])?([0-9]+)/m);
-        var flag = false; 
-        var s=0;
-        while(s<str.length && str[s]==' ')
+    var arr = str.trim().match(/^([+-])?([0-9]+)/);
+    if(arr)
+    {   
+        console.log('this is string: '+arr)
+        var s = arr[1];
+        var n = parseInt(arr[2]);
+        console.log(s)
+        if (s) 
         {
-            s++;
-        }
-        if(s<str.length && str[s]=='+')
-        {
-            s++;
-        }else
-        {
-            if(s<str.length && str[s]=='-')
+            if (s == '-') 
             {
-                a=true;
-                s++;
+                n = -1 * n;
+                return Math.max(-2147483648,n);
             }
         }
-        for()
-        if (arr) {
-            var sign = arr[1];
-            var strNum = arr[2];
-            var number = parseInt(strNum);
-            if (sign) {
-                if (sign == '-') {
-                    number = -1 * number;
-                    return Math.max(-2147483648, number);
-                }
-            }
-            return Math.min(2147483647, number);
-        } else {
-            return 0;
-        }
+        return Math.min(2147483647, n);
+    }
+    else
+    {
+        return 0;
+    }
 };
-console.log(myAtoi('+123'));
+
+console.log(myAtoi(' 123'));
+console.log(myAtoi('-123'));
+console.log(myAtoi('123'));
+console.log(myAtoi(' -123'));
