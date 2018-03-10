b12d78f498542f93cb5ec6c7056066ae030ab80a
lmislm <wszdlm@gmail.com>
2018-03-10 20:08:04 +0800

    method more

--- code-interview/reversestring/index.js
+++ code-interview/reversestring/index.js
@@ -2,17 +2,16 @@
  * @Author: lmislm 
  * @Date: 2018-03-10 11:18:05 
  * @Last Modified by: lmislm
- * @Last Modified time: 2018-03-10 19:21:47
+ * @Last Modified time: 2018-03-10 19:27:03
  */
 
 
 // revese("张三") === "三张";
 // revese("李四jest") === "tsej四李";
 
+//方法三
 function reverse(str) {
-    return str.split('').reduce((reversed, character) => {
-        return character + reversed;
-   },'');
+    return str.split('').reduce((reversed, character) => character + reversed,'');
 }
 
 module.exports = reverse;
