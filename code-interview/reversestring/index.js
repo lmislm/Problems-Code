b57dc520618044f2eb4e05c07b3fa7ba180c15dc
lmislm <wszdlm@gmail.com>
2018-03-10 19:23:12 +0800

    reduce 方法

--- code-interview/reversestring/index.js
+++ code-interview/reversestring/index.js
@@ -2,7 +2,7 @@
  * @Author: lmislm 
  * @Date: 2018-03-10 11:18:05 
  * @Last Modified by: lmislm
- * @Last Modified time: 2018-03-10 12:49:52
+ * @Last Modified time: 2018-03-10 19:21:47
  */
 
 
@@ -10,7 +10,31 @@
 // revese("李四jest") === "tsej四李";
 
 function reverse(str) {
-    return str.split('').reverse().join('')
+    return str.split('').reduce((reversed, character) => {
+        return character + reversed;
+   },'');
 }
 
 module.exports = reverse;
+
+//方法二
+// function reverse(str) {
+//     let reversed = '';
+
+//     for (let character of str) {
+
+//         // reversed = character + reversed is wrong;
+//         reversed = character + reversed;
+//     }
+
+//     return reversed;
+// }
+
+
+// 方法一
+// function reverse(str) {
+//   return str
+//     .split("")
+//     .reverse()
+//     .join("");
+// }
