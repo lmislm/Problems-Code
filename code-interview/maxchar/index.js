c74af1145bde6bd104b67ba328c86f5b0ec0c78c
lmislm <wszdlm@gmail.com>
2018-03-12 22:26:38 +0800

    1st

--- code-interview/maxchar/index.js
+++ code-interview/maxchar/index.js
@@ -2,7 +2,7 @@
  * @Author: lmislm 
  * @Date: 2018-03-11 20:31:12 
  * @Last Modified by: lmislm
- * @Last Modified time: 2018-03-11 20:49:44
+ * @Last Modified time: 2018-03-12 22:23:32
  */
 
 //  给出一串字符返回其中出现 次数最多 的符号
@@ -10,8 +10,21 @@
 // maxChar("abbbcde") === "b"
 // maxChar("cdfsss 1111223") === "1"
 
-function maxChar(str) {
+// function maxChar(str) {
     
-}
+// }
 
-module.exports = maxChar;
+// module.exports = maxChar;
+
+
+const string = "hello there!"
+const chars = {};
+
+for(let char of string) {
+    if(!chars[char]) {
+        chars[char] = 1;
+    } else {
+        chars[char]++;
+    }
+}
+console.log(chars)
