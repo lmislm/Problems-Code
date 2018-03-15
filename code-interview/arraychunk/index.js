92d453db142cc8ac6cfbe92d7efd274571a10574
lmislm <wszdlm@gmail.com>
2018-03-15 21:14:28 +0800

    2nd

--- code-interview/arraychunk/index.js
+++ code-interview/arraychunk/index.js
@@ -2,7 +2,7 @@
  * @Author: lmislm 
  * @Date: 2018-03-13 16:49:17 
  * @Last Modified by: lmislm
- * @Last Modified time: 2018-03-15 20:49:33
+ * @Last Modified time: 2018-03-15 21:13:42
  */
 
 //数组分割
@@ -13,19 +13,34 @@
 // arraychunk([1,2,3,4,5,6,7,8], 3) --> [[1,2,3], [4,5,6],[7,8]]
 
 function arraychunk(array, size) {
+    //slice(start(include)_point, end(noinclude)_point)
     const chunked = [];
+    let index = 0;
 
-    for(let element of array) {
-        const last = chunked[chunked.length - 1];
-
-        if(!last || last.length === size) {
-            chunked.push([element]);
-        } else {
-            last.push(element);
-        }
+    while(index < array.length) {
+        chunked.push(array.slice(index, index + size));
+        index += size;
     }
 
     return chunked;
 }
+
+
+//方法一
+// function arraychunk(array, size) {
+//     const chunked = [];
+
+//     for(let element of array) {
+//         const last = chunked[chunked.length - 1];
+
+//         if(!last || last.length === size) {
+//             chunked.push([element]);
+//         } else {
+//             last.push(element);
+//         }
+//     }
+
+//     return chunked;
+// }
 
 module.exports = arraychunk;
