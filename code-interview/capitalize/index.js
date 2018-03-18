4a93de040e35b506b7b394fabef4eed4497e41ba
lmislm <wszdlm@gmail.com>
2018-03-18 22:36:22 +0800

    capitalize 2nd

--- code-interview/capitalize/index.js
+++ code-interview/capitalize/index.js
@@ -4,14 +4,33 @@
 // capitalize('a little') === A Little
 // capitalize('my name is lmislm') === My Name Is Lmislm
 
+// 方法二：通过空格i判断首字母
 function capitalize(str) {
-    const words = [];
-    // words.split('')
-    for(let word of str.split(' ')) {
-        // console.log(word)
-        words.push(word[0].toUpperCase() + word.slice(1));
-    }
-    return words.join(' ');
+//    const firstLetter = str[0].toUpperCase();
+   let result = str[0].toUpperCase();
+
+   for(let i = 1; i < str.length; i++) {
+       if(str[i - 1] === ' ') {
+           result += str[i].toUpperCase();
+       }else {
+           result += str[i];
+       }
+   }
+
+   return result;
 }
 
 module.exports = capitalize;
+
+//方法一: 将词组分开
+// function capitalize(str) {
+//     const words = [];
+//     // words.split('')
+//     for(let word of str.split(' ')) {
+//         // console.log(word)
+//         words.push(word[0].toUpperCase() + word.slice(1));
+//     }
+//     return words.join(' ');
+// }
+
+// module.exports = capitalize;
