2a1199ffe3140e5ca3a84f45491d2f4cfa7b8529
lmislm <wszdlm@gmail.com>
2018-03-18 20:18:17 +0800

    init

---
+++ code-interview/capitalize/index.js
@@ -0,0 +1,9 @@
+// ------ 要求
+// 写一个函数，输入字符串，要求将其每个词的首字母变为大写
+// 例子
+// capitalize('a little') === A Little
+// capitalize('my name is lmislm') === My Name Is Lmislm
+
+function capitalize(str) {}
+
+module.exports = capitalize;
---
+++ code-interview/capitalize/test.js
@@ -0,0 +1,18 @@
+const capitalize = require('./index');
+
+test('函数已定义', () => {
+    expect(typeof capitalize).toEqual('function');
+});
+
+
+test('字母大写化', () => {
+    expect(capitalize('we need to create a new')).toEqual(
+        'We Need To Create A New'
+    );
+});
+
+test('词首字母大写', () => {
+    expect(capitalize('ma name is lmislm')).toEqual(
+        'My Name Is Lmislm'
+    );
+});
