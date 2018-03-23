98c3cb851c5cabca6589077551b9a3674e4c7bea
lmislm <wszdlm@gmail.com>
2018-03-23 22:11:55 +0800

    1st

--- code-interview/pyramids/index.js
+++ code-interview/pyramids/index.js
@@ -2,7 +2,7 @@
  * @Author: lmislm 
  * @Date: 2018-03-23 18:45:36 
  * @Last Modified by: lmislm
- * @Last Modified time: 2018-03-23 19:17:51
+ * @Last Modified time: 2018-03-23 22:09:11
  */
 //给出一个数字n,打印输出金字塔，如下：
 // --------- 例如 ----
@@ -15,8 +15,22 @@
 //      '  *  '
 //      ' *** '
 //      '*****'
-//column 行；row 列；
+//column ：列数；row 行数；
 
-function pyramid(n) {}
+function pyramid(n) {
+  const midpoint = Math.floor((2 * n - 1) / 2);
+  for (let row = 0; row < n; row++) {
+    let level = '';
+
+    for (let column = 0; column < 2 * n - 1; column++) {
+      if (midpoint - row <= column && midpoint + row >= column) {
+        level += '*';
+      } else {
+        level += ' ';
+      }
+    }
+    console.log(level);
+  }
+}
 
 module.exports = pyramid;
