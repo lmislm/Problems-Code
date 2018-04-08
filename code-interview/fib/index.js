9cfda0bc12198ca209c43ffaf215016956b0ebc1
lmislm <wszdlm@gmail.com>
2018-04-08 10:12:47 +0800

    递归方法解决fibonacci

--- code-interview/fib/index.js
+++ code-interview/fib/index.js
@@ -3,18 +3,26 @@
 // 后一位数是前两位数之和
 // (例如fib(2) === 0 + 1) === 1;
 
-//迭代法
+//递归方法
 function fib(n) {
-  const result = [0, 1];
-
-  for (i = 2; i <= n; i++) {
-    const a = result[i - 1];
-    const b = result[i - 2];
-
-    result.push(a + b);
+  if (n < 2) {
+    return n;
   }
-
-  return result[n];
+  return fib(n - 1) + fib(n - 2)
 }
+
+//----------迭代法
+// function fib(n) {
+//   const result = [0, 1];
+
+//   for (i = 2; i <= n; i++) {
+//     const a = result[i - 1];
+//     const b = result[i - 2];
+
+//     result.push(a + b);
+//   }
+
+//   return result[n];
+// }
 
 module.exports = fib;
